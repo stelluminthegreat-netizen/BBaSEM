d7c6f69e78957cfb529c6f7c5204f88fdc8f9336
Stellumin <stelluminthegreat@gmail.com>
2026-08-23 11:54:28 -0400

    Add wait for server framework loader to finish

--- Game/server/Loader.server.lua
+++ Game/server/Loader.server.lua
@@ -1,8 +1,11 @@
-shared["Classes"] = {}
+task.wait()
+local loaded = game.ReplicatedStorage.Values.ServerFrameworkLoaded
+while loaded.Value == false do task.wait() end
+if not shared.GameClasses then shared["GameClasses"] = {} end
 
 for index, descendant in script.Parent:GetDescendants() do
-	if descendant.ClassName == "ModuleScript" then shared.Classes[descendant.Name] = require(descendant) end
+	if descendant.ClassName == "ModuleScript" then shared.GameClasses[descendant.Name] = require(descendant) end
 end
 
-for index, module in shared.Classes do if type(module.initialize) == "function" and index ~= "Libraries" then module.initialize() end end
-for index, module in shared.Classes do if type(module.start) == "function" and index ~= "Libraries" then task.defer(module.start) end end
+for index, module in shared.GameClasses do if type(module.initialize) == "function" and index ~= "Libraries" then module.initialize() end end
+for index, module in shared.GameClasses do if type(module.start) == "function" and index ~= "Libraries" then task.defer(module.start) end end
