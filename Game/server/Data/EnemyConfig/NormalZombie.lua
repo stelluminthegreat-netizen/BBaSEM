0bfcc7a8da89bd809f917432a027d2a73444a86e
Stellumin <stelluminthegreat@gmail.com>
2026-08-23 12:03:51 -0400

    Add NormalZombie config

---
+++ Game/server/Data/EnemyConfig/NormalZombie.lua
@@ -0,0 +1,9 @@
+return {
+    Health = 100,
+    Damage = 10,
+    WalkSpeed = 1,
+    AttackSpeed = 1,
+    Hitbox = Vector3.new(3, 3, 3),
+    CurrentTarget = "",
+    DetectRange = Vector3.new(1000, 1000, 1000)
+}
