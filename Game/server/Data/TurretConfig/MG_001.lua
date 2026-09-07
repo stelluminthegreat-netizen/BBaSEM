806d572f3fc650d3eb2b77c4c7e9c206c4cb6ae6
Stellumin <stelluminthegreat@gmail.com>
2026-09-07 03:11:08 -0400

    Add Conns, Events, EventNames, & Status

--- Game/server/Data/TurretConfig/MG_001.lua
+++ Game/server/Data/TurretConfig/MG_001.lua
@@ -1,6 +1,15 @@
 return {
     AttackSpd = 4,
     Health = 150,
-    Dmg = 5,
+    Dmg = 10,
     Range = 2,
+    Conns = {},
+    Events = {},
+    EventNames = {
+        [1] = "Destroyed",
+        [2] = "Died",
+    },
+    Status = {
+        Attacking = false
+    },
 }
