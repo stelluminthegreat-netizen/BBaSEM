1e197bf83348c430f3c169839b61fc84ccfcc61e
Stellumin <stelluminthegreat@gmail.com>
2026-09-11 06:41:35 -0400

    Add Fabricator and Economy
    Economy handles the overall cash.
    Storing and Modifying.
    
    Fabricators are structures that increments
    the Cash in Economy.
    Mainly a money generating structure.

---
+++ Game/server/Data/FabricatorConfig/Fabricator_T1.lua
@@ -0,0 +1,11 @@
+return {
+    GenerationRate = -5,
+    GenerationInterval = 1,
+    Health = 100,
+    EventNames = {
+        [1] = "Died",
+        [2] = "Destroyed"
+    },
+    Events = {},
+    Conns = {},
+}
