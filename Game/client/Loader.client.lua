ba71a1b128ee20d29328cddfd4a1978d6e5ae067
Stellumin <stelluminthegreat@gmail.com>
2026-09-13 04:42:02 -0400

    Let client Loader wait for Framework client loader
    Game Module Laoder for Client waits for
    the Framework's client Module Loader to fully load
    before loading it modules

--- Game/client/Loader.client.lua
+++ Game/client/Loader.client.lua
@@ -1,8 +1,12 @@
-shared["Classes"] = {}
-
-for index, descendant in script.Parent:GetDescendants() do
-	if descendant.ClassName == "ModuleScript" then shared.Classes[descendant.Name] = require(descendant) end
+while not shared.Loaded do
+	task.wait()
 end
 
-for index, module in shared.Classes do if type(module.initialize) == "function" and index ~= "Libraries" then module.initialize() end end
-for index, module in shared.Classes do if type(module.start) == "function" and index ~= "Libraries" then task.defer(module.start) end end
+shared["GameClasses"] = {}
+
+for index, descendant in script.Parent:GetDescendants() do
+	if descendant.ClassName == "ModuleScript" then shared.GameClasses[descendant.Name] = require(descendant) end
+end
+
+for index, module in shared.GameClasses do if type(module.initialize) == "function" and index ~= "Libraries" then module.initialize() end end
+for index, module in shared.GameClasses do if type(module.start) == "function" and index ~= "Libraries" then task.defer(module.start) end end
